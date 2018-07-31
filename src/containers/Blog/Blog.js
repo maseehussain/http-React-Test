4338e8f7baa83a8e22c56020451674be59fab087
Masee Hussain <hussainmasee@gmail.com>
2018-07-31 14:08:00 +0100

    accessed api with axios and got response data displaying on console

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios from "axios";
 
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
@@ -6,6 +7,12 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends React.Component {
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div>
