8ff4b3997e5427331bc5bab2d551b7f33b2b72af
Masee Hussain <hussainmasee@gmail.com>
2018-08-02 09:12:37 +0100

    used .map to access the data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -20,16 +20,12 @@ class Blog extends React.Component {
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post />;
+      return <Post key={post.id} title={post.title} />;
     });
 
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
