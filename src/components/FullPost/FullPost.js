0ba766dca4aff79f0dac6d2fc07dc06d6d335414
Masee Hussain <hussainmasee@gmail.com>
2018-08-10 14:56:55 +0100

    We an if statement to get all data at the same time

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -26,15 +26,20 @@ class FullPost extends React.Component {
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    //33- We get an error message because the request is a promise first we the id the the post data
+    //we need a if request to show a loading message
+    if (this.props.id) {
+      let post = <p style={{ textAlign: "center" }}>Loading...</p>;
+    }
     //25- add an if statement which reads if it is trueish(null is treated as false)
     //then output the post.
-
     //31- then here we put {loadedPost.title}, {loadedPost.body}
+    //32- We need to use this.state
     if (this.props.id) {
       post = (
         <div className="FullPost">
-          <h1>{loadedPost.title}</h1>
-          <p>{loadedPost.body}</p>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
