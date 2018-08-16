277dd9181e66a37468afc2e18cc2ca434f106c02
Masee Hussain <hussainmasee@gmail.com>
2018-08-16 11:42:18 +0100

    Using global and instances for configuration

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 //50- Using instances of the global configurations
 const instance = Axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com";
+  baseURL: "https://jsonplaceholder.typicode.com"
 });
 
 instance.defaults.headers.common["Authorization"] = "AUTH TOKEN FROM INSTANCE";
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React from "react";
 //3- Import Axios
 // import Axios from "axios";
-
+import Axios from "../../Axios";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
