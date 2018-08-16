bbc51a8c368558f4ffbd99a0dfb48a06f7a2856c
Masee Hussain <hussainmasee@gmail.com>
2018-08-16 09:38:28 +0100

    using interceptors to access my request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+//46- import axios
+import Axios from "axios";
+
+//47- accessing the interceptors
+Axios.interceptors.request.use(req => {
+  console.log(req);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
