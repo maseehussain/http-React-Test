749fee385172551f2335866837f3481eece9200f
Masee Hussain <hussainmasee@gmail.com>
2018-08-17 11:57:53 +0100

    moved post JSX from blog to to our posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,15 +5,6 @@ import Axios from "../../Axios";
 import "./Blog.css";
 
 class Blog extends React.Component {
-  //7- Created a state for my posts where I can manage them.
-  //23- We create that selectedPostId in out state with a null value
-  //47- You can update the state with err: false to show someting went wrong
-  state = {
-    posts: [],
-    selectedPostId: null,
-    err: false
-  };
-
   //1-Install Axios
   //2- Implement componentDidMount method
   componentDidMount() {
@@ -50,28 +41,6 @@ class Blog extends React.Component {
   };
 
   render() {
-    //49- Instead of creatinfg a constant we create a varaible post with an if statement
-    //if it is true it display
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-    if (!this.state.err) {
-      //8- Set my setState to the post and map through them
-      posts = this.state.posts.map(post => {
-        //9- I return my JSX here
-        //12- need to set a title property here and pass through {post.title}
-        //13- also add the key prop which makes each post unique
-        //18- I pass {post.author}
-        //20- In the blog container I now need to pass that click prop to the post
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     //10- We output our array of {posts}
     //22- id={this.state.selectedPostId} gets passed to my FullPosts component
     return (
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class Posts extends React.Component {
+  //7- Created a state for my posts where I can manage them.
+  //23- We create that selectedPostId in out state with a null value
+  //47- You can update the state with err: false to show someting went wrong
+  state = {
+    posts: [],
+    selectedPostId: null,
+    err: false
+  };
+
+  render() {
+    //49- Instead of creatinfg a constant we create a varaible post with an if statement
+    //if it is true it display
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.err) {
+      //8- Set my setState to the post and map through them
+      posts = this.state.posts.map(post => {
+        //9- I return my JSX here
+        //12- need to set a title property here and pass through {post.title}
+        //13- also add the key prop which makes each post unique
+        //18- I pass {post.author}
+        //20- In the blog container I now need to pass that click prop to the post
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
