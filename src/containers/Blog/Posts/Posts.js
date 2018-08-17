e52a3ff695434db4a6a5dee67c29641d9181ffc2
Masee Hussain <hussainmasee@gmail.com>
2018-08-17 12:11:26 +0100

    removed css for post into Posts.css and imported file

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -3,6 +3,8 @@ import Axios from "../../../Axios";
 
 import Post from "../../../components/Post/Post";
 
+import "./Posts.css";
+
 class Posts extends React.Component {
   //7- Created a state for my posts where I can manage them.
   //23- We create that selectedPostId in out state with a null value
