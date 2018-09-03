6556c2ec7c8c8d3ebc8c6c7ba54eb18a565d5f6b
Masee Hussain <hussainmasee@gmail.com>
2018-09-03 10:46:12 +0100

    reverted Post.js back

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,18 +2,16 @@ import React from "react";
 
 import "./Post.css";
 
-const post = props => {
+const post = props => (
   //11- We add the title from our posts data with {props.title}
   //17- We access {props.author}
   //19- I added a onClick listener to my componet {props.clicked}
-  return (
-    <article className="Post" onClick={props.clicked}>
-      <h1>{props.title}</h1>
-      <div className="Info">
-        <div className="Author">{props.author}</div>
-      </div>
-    </article>
-  );
-};
+  <article className="Post" onClick={props.clicked}>
+    <h1>{props.title}</h1>
+    <div className="Info">
+      <div className="Author">{props.author}</div>
+    </div>
+  </article>
+);
 
 export default post;
