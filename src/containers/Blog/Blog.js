b22e92500c717338fa5a2c41926bd42a1891dc80
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 09:42:15 +0100

    comment out the import for NewPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import React from "react";
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+// import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends React.Component {
