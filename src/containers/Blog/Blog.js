b6e73827e1035ef34d0a65e3d5875f43a819611e
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 09:45:31 +0100

    import asyncComponent to be able to code splitting

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,9 @@ import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import Posts from "./Posts/Posts";
 // import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
+import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = asyncComponent();
 
 class Blog extends React.Component {
   state = {
